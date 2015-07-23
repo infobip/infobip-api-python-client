5815dc579ac2e8c9ad077d82dd74a466437d888e
Nikola Marić <nikola.maric@infobip.com>
2015-07-23 16:36:09 +0200

    Using pypandoc to generate long description.

--- setup.py
+++ setup.py
@@ -18,15 +18,26 @@ import distutils.core as mod_distutilscore
 
 from setuptools import find_packages
 
+# Try to parse README.md and convert it to rst format
+# which can be used on PyPi. Using Pandoc & PyPandoc.
+try:
+    import pypandoc
+    long_description = '\n\n' + pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = '\n\n' + open('README.md').read()
+
+# Properly register all python source files from package
 my_packages = find_packages()
 
 mod_distutilscore.setup(
     name='infobip-api-python-client',
-    version='0.0.1',
-    description='Infobip API client library in Python',
+    version='0.0.3',
+    description='Infobip SMS API client library in Python',
+    long_description=long_description,
     license='Apache License, Version 2.0',
     author='Infobip Ltd.',
     author_email='plugins@infobip.com',
+    maintainer_email='plugins@infobip.com',
     url='https://github.com/infobip/infobip-api-python-client',
     packages=my_packages,
     package_dir={'infobip_api_python_client': 'infobip_api_python_client'},
