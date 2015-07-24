f62d231c36ba8d61ff93a5d9c0096237873cc9c7
mmatosevic <matija.matosevic@infobip.com>
2015-07-24 16:45:58 +0200

    base_url is "https://api.infobip.com" by default

--- examples/__init__.py
+++ examples/__init__.py
@@ -2,8 +2,7 @@ __author__ = 'nmaric'
 
 from infobip.util.configuration import Configuration
 
-infobip_base_url = "https://api.infobip.com"
 infobip_username = "username"
 infobip_password = "password"
 
-configuration = Configuration(infobip_base_url, infobip_username, infobip_password)
+configuration = Configuration(infobip_username, infobip_password)
