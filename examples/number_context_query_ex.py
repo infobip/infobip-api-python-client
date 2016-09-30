b4b1fa3cda96cf650e7738caf64d01fa70204b75
jantolis <josip.antolis@infobip.com>
2016-09-30 13:15:43 +0200

    added number context examples

---
+++ examples/number_context_query_ex.py
@@ -0,0 +1,11 @@
+from infobip.clients import number_context_query
+from infobip.api.model.nc.query.NumberContextRequest import NumberContextRequest
+from __init__ import configuration
+
+number_context_client = number_context_query(configuration)
+
+request = NumberContextRequest()
+request.to = "number1aaa"
+response = number_context_client.execute(request)
+
+print(response)
