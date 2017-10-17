306a59c17679d365012c92b68445e97374c4859b
hkozacinski <hrvoje.kozacinski@infobip.com>
2017-10-17 15:19:05 +0200

    added message count and pending message count parameters to MOReportResponse class

--- infobip/api/model/sms/mo/reports/MOReportResponse.py
+++ infobip/api/model/sms/mo/reports/MOReportResponse.py
@@ -9,6 +9,32 @@ from infobip.api.model.sms.mo.reports.MOReport import MOReport
 
 class MOReportResponse(DefaultObject):
     @property
+    @serializable(name="messageCount", type=int)
+    def message_count(self):
+        return self.get_field_value("message_count")
+
+    @message_count.setter
+    def message_count(self, message_count):
+        self.set_field_value("message_count", message_count)
+
+    def set_message_count(self, message_count):
+        self.message_count = message_count
+        return self
+
+    @property
+    @serializable(name="pendingMessageCount", type=int)
+    def pending_message_count(self):
+        return self.get_field_value("pending_message_count")
+
+    @pending_message_count.setter
+    def pending_message_count(self, pending_message_count):
+        self.set_field_value("pending_message_count", pending_message_count)
+
+    def set_pending_message_count(self, pending_message_count):
+        self.pending_message_count = pending_message_count
+        return self
+
+    @property
     @serializable(name="results", type=MOReport, list=True)
     def results(self):
         return self.get_field_value("results")
